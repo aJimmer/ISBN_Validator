5ca48bd0b667494af519e4a6442c1a2893d5662d
Angel Jimenez <ajl890889702@csu.fullerton.edu>
2019-10-19 22:30:46 -0700

    Initial Commit

---
+++ src/com/ajimmer/isbntools/ValidateISBN.java
@@ -0,0 +1,12 @@
+package com.ajimmer.isbntools;
+
+public class ValidateISBN {
+
+	public boolean checkISBN(int isbn) {
+		if (isbn == "0140449116") {
+			return true;
+		}
+		return false;
+	}
+	
+}
